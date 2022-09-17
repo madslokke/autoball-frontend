130e042ea082a443e0715a980f3d67ad2b4af5e4
Mads Mikkelsen <madslokke@gmail.com>
2022-09-18 00:51:46 +0200

    Show error

--- util/api.ts
+++ util/api.ts
@@ -22,7 +22,7 @@ export default function api() {
 
     console.error(error)
 
-    return Promise.reject({status: error.response?.status, errors: ['Oops!']})
+    return Promise.reject({status: error.response?.status, ...error})
   })
 
   return api
